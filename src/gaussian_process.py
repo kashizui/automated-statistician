fa50a1da2717e42b3b768de6c427bf9442565d39
Rui Shu <00ruishu@gmail.com>
2015-11-25 03:17:08 -0800

    Include 2d plot in unit test

--- src/gaussian_process.py
+++ src/gaussian_process.py
@@ -89,7 +89,7 @@ class GaussianProcess(object):
                 mini_y = y[j*self.batch_size : (j+1)*self.batch_size]
                 self.sess.run(train, feed_dict={self.X: mini_X, self.y: mini_y})
         # Current hack: save the values
-        self.K_invf = self.sess.run(self.K_inv, feed_dict={self.X: mini_X, self.y: mini_y})
+        self.K_invf = self.sess.run(self.K_inv, feed_dict={self.X: X, self.y: y})
         self.Xf = X
         self.yf = y
         
@@ -98,34 +98,36 @@ class GaussianProcess(object):
         K_ = self.kernel.covariance(X, self.Xf)
         K_K_invf = tf.matmul(K_, self.K_invf)
         y_pred = tf.matmul(K_K_invf, self.yf)
-        var = self.kernel.covariance(X) - tf.matmul(K_K_invf, tf.transpose(K_))
-        return self.sess.run(y_pred), np.diag(self.sess.run(var)).reshape([X.shape[0],1])
-        
-def main():        
-    from scipy.spatial.distance import cdist
+        cov = self.kernel.covariance(X) - tf.matmul(K_K_invf, tf.transpose(K_))
+        return y_pred, cov
+
+    
+def main_1d():        
     import matplotlib.pyplot as plt
     # Create X and Y
-    n_samples = 10
+    n_samples = 50
     n_dim = 1
     X = np.float32(np.random.uniform(1, 10, [n_samples, n_dim]))
     y = np.float32((np.sin(X.sum(1)).reshape([n_samples, 1]) +
          np.random.normal(0,.1, [n_samples, 1])))
     kernel = SquaredExponential(n_dim=n_dim,
-                                init_scale_range=(1.,1.),
+                                init_scale_range=(.1,.2),
                                 init_amp=1.)
     gp = GaussianProcess(n_epochs=100,
-                         batch_size=n_samples,
+                         batch_size=10,
                          n_dim=n_dim,
                          kernel=kernel,
                          noise=0.1,
                          train_noise=False,
-                         optimizer=tf.train.GradientDescentOptimizer(0.01),
+                         optimizer=tf.train.GradientDescentOptimizer(0.001),
                          verbose=0)
     gp.fit(X, y)
-    print gp.sess.run(gp.noise)
+    print gp.sess.run(gp.kernel.length_scales)
     X_new = np.float32(np.random.uniform(1, 10, [n_samples*100, n_dim]))
     X_new = np.sort(X_new, axis=0)
-    y_pred, var = gp.predict(X_new)
+    y_pred, cov = gp.predict(X_new)
+    y_pred = gp.sess.run(y_pred)
+    var = np.diag(gp.sess.run(cov)).reshape(y_pred.shape)
     ci = np.sqrt(var)*2
     plt.plot(X_new, y_pred)
     plt.plot(X_new, y_pred+ci, 'g--')
@@ -133,5 +135,50 @@ def main():
     plt.scatter(X, y)
     plt.show()
     
+def main_2d():        
+    from mpl_toolkits.mplot3d import Axes3D
+    from matplotlib import cm
+    from matplotlib.ticker import LinearLocator, FormatStrFormatter
+    import matplotlib.pyplot as plt
+
+    n_samples = 50
+    n_dim = 2
+    
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    X = np.float32(np.random.uniform(0, 10, [n_samples, n_dim]))
+    y = np.float32((np.sin(np.sqrt(X).sum(1)).reshape([n_samples, 1])
+                    + np.random.normal(0, .1, [n_samples, 1])))
+    scat = ax.scatter(X[:,0], X[:,1], y)
+
+    kernel = SquaredExponential(n_dim=n_dim,
+                                init_scale_range=(.1,.2),
+                                init_amp=1.)
+    gp = GaussianProcess(n_epochs=100,
+                         batch_size=10,
+                         n_dim=n_dim,
+                         kernel=kernel,
+                         noise=0.1,
+                         train_noise=False,
+                         optimizer=tf.train.GradientDescentOptimizer(0.001),
+                         verbose=0)
+    gp.fit(X, y)
+    a = np.arange(0, 10, 0.25)
+    b = np.arange(0, 10, 0.25)
+    a, b = np.meshgrid(a, b)
+    X_new = np.float32(np.hstack((a.ravel().reshape(-1, 1), b.ravel().reshape(-1, 1))))
+    y_pred, cov = gp.predict(X_new)
+    y_pred = (gp.sess.run(y_pred)).reshape(a.shape)
+    var = np.diag(gp.sess.run(cov)).reshape(y_pred.shape)
+    ci = (np.sqrt(var)*2).reshape(a.shape)
+    surf = ax.plot_surface(a, b, y_pred, rstride=1, cstride=1, cmap=cm.coolwarm,
+                           linewidth=0, antialiased=False, alpha=.5)
+    surf = ax.plot_surface(a, b, y_pred-ci, rstride=1, cstride=1, cmap=cm.cool,
+                           linewidth=0, antialiased=False, alpha=.5)
+    surf = ax.plot_surface(a, b, y_pred+ci, rstride=1, cstride=1, cmap=cm.cool,
+                           linewidth=0, antialiased=False, alpha=.5)
+    plt.show()
+
+    
 if __name__ == "__main__":
-    main()
+    main_2d()
