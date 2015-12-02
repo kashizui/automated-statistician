0f3718c0f18f4225340e071424b319cdf6fc5f86
Rui Shu <00ruishu@gmail.com>
2015-12-01 23:19:32 -0800

    Modify unit test

--- src/bayesian_optimizer.py
+++ src/bayesian_optimizer.py
@@ -21,7 +21,7 @@ class BayesianOptimizer(object):
     posterior mean and variance of the latent function given observed data. The
     goal of bayesian optimization is to find the maximum of the latent function.
     """
-    def __init__(self, gp, region, iters,
+    def __init__(self, gp, region, iters, tries,
                  optimizer=tf.train.GradientDescentOptimizer(0.1),
                  verbose=0):
         """ Initialize the parameters of the BayesianOptimizer object
@@ -48,6 +48,7 @@ class BayesianOptimizer(object):
         # Define optimization algorithm
         self.opt = optimizer
         self.iters = iters
+        self.n_tries = tries
         self.verbose = verbose
 
         # FIXME
@@ -119,6 +120,19 @@ class BayesianOptimizer(object):
                         self.gp.y : self.gp.yf,
                         self.gp.np_K_inv : self.gp.K_invf}
     def select(self):
+        max_acq = -np.inf
+        for _ in xrange(self.n_tries):
+            x_cand, y_cand, acq_cand = self._select()
+            # print "candidate_profile:", x_cand, y_cand, acq_cand
+            if acq_cand > max_acq:
+                x_next = x_cand
+                y_next = y_cand
+                acq_next = acq_cand
+                max_acq = acq_cand
+        # print x_next, y_next, acq_next
+        return x_next, y_next, acq_next
+            
+    def _select(self):
         """ Select a point that maximizes the acquisition function. Finds this
         point via gradient descend of -acq.
 
@@ -134,8 +148,8 @@ class BayesianOptimizer(object):
         # Only need to initialize self.x
         self.sess.run(tf.initialize_variables([self.x]))
         if self.verbose > 0:
-            print "BOPoint:", self.sess.run(self.x)[0]
-            print "BOAcquisition:", self.sess.run(self.acq, feed_dict=self.gp_dict)[0,0]
+            print "BOInitPoint:", self.sess.run(self.x)[0]
+            print "BOInitAcquisition:", self.sess.run(self.acq, feed_dict=self.gp_dict)[0,0]
         for _ in xrange(self.iters):
             self.sess.run(self.train, feed_dict=self.gp_dict)
             # Break from gradient descent if outside of optimization region
@@ -155,68 +169,65 @@ def main_1d():
     from gaussian_process import GaussianProcess
     import matplotlib.pyplot as plt
     import time
+    np.random.seed(10)
+    tf.set_random_seed(10)
     # Settings
-    n_samples = 20
+    n_samples = 100
     batch_size = 4
-    new_samples = 100
+    new_samples = 1000
     n_dim = 1
     # Set up the modules for bayesian optimizer
     kernel = SquaredExponential(n_dim=n_dim,
-                                init_scale_range=(.5,1),
+                                init_scale_range=(.1,.5),
                                 init_amp=1.)
     gp = GaussianProcess(n_epochs=100,
                          batch_size=10,
                          n_dim=n_dim,
                          kernel=kernel,
-                         noise=0.1,
+                         noise=0.05,
                          train_noise=False,
                          optimizer=tf.train.GradientDescentOptimizer(0.001),
                          verbose=0)
-    bo = BayesianOptimizer(gp, region=np.array([[0., 3.]]),
+    bo = BayesianOptimizer(gp, region=np.array([[0., 1.]]),
                            iters=100,
+                           tries=4,
                            optimizer=tf.train.GradientDescentOptimizer(0.1),
-                           verbose=0)
+                           verbose=1)
     # Define the latent function + noise
     def observe(X):
-        y = np.float32((np.sin(X.sum(1)).reshape([X.shape[0], 1]) +
-                        np.random.normal(0,.1, [X.shape[0], 1])))
+        y = np.float32(
+            1*(-(X - 0.5) ** 2 + 1) +
+            np.random.normal(0,.1, [X.shape[0], 1]))
+        # y = np.float32((np.sin(X.sum(1)).reshape([X.shape[0], 1]) +
+        #                 np.random.normal(0,.1, [X.shape[0], 1])))
         return y
     # Get data
     X = np.float32(np.random.uniform(0, 1, [n_samples, n_dim]))
     y = observe(X)
-    plt.axis((0, 3, -3, 3))
+    plt.axis((-0.1, 1.1, 0, 1.5))
     # Fit the gp
     bo.fit(X, y)
-    for i in xrange(100):
+    for i in xrange(10):
         # print "Iteration {0:3d}".format(i) + "*"*80
         t0 = time.time()
         max_acq = -np.inf
         # Inner loop to allow for gd with random initializations multiple times
-        for _ in xrange(1):
-            t1 = time.time()
-            # Get the best point w.r.t. acquisition function.
-            x_cand, y_cand, acq_cand = bo.select()
-            print "BOSelectDuration: {0:.5f}".format(time.time() - t1)
-            if acq_cand > max_acq:
-                # Save best point
-                x_next = x_cand
-                y_next = y_cand
-                acq_next = acq_cand
+        x_next, y_next, acq_next = bo.select()
         # Plot the selected point
         plt.plot([x_next[0,0], x_next[0,0]], plt.ylim(), 'r--')
         plt.scatter(x_next, y_next, c='r', linewidths=0, s=50)
         plt.scatter(x_next, acq_next, c='g', linewidths=0, s=50)
         # Observe and add point to observed data
-        # y_obs = observe(x_next)
-        # X = np.vstack((X, x_next))
-        # y = np.vstack((y, y_obs))
+        y_obs = observe(x_next)
+        X = np.vstack((X, x_next))
+        y = np.vstack((y, y_obs))
         t2 = time.time()
         # Fit again
         bo.fit(X, y)
-        # print "BOFitDuration: {0:.5f}".format(time.time() - t2)
-        # print "BOTotalDuration: {0:.5f}".format(time.time() - t0)
+        print "BOFitDuration: {0:.5f}".format(time.time() - t2)
+        print "BOTotalDuration: {0:.5f}".format(time.time() - t0)
     # Get the final posterior mean and variance for the entire domain space
-    X_new = np.float32(np.linspace(0, 3, new_samples).reshape(-1, 1))
+    X_new = np.float32(np.linspace(0, 1, new_samples).reshape(-1, 1))
     X_new = np.sort(X_new, axis=0)
     y_pred, var = gp.np_predict(X_new)
     # Compute the confidence interval
